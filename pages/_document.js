de140efd73270ac1b68f02c5b8618cec8d4e617c
doanthan <66489941+doanthan@users.noreply.github.com>
2025-01-13 16:31:06 +1100

    no scripts

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,6 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <script
-                        async type="text/javascript"
-                        src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=VZdSUY"
-                    ></script>
-                    <script
-                        async type="text/javascript"
-                        src="https://cdn.kaypush.com/js/script.js?company_id=Op1vnuw&site=app4"
-                    ></script>
                     <link
                         rel="stylesheet"
                         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
